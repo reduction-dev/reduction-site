5c5de99ed7d3cf73dcc871eab6622bfbc3c47ece
Mitch Lloyd <mitch.lloyd@gmail.com>
2025-03-03 15:30:29 -0500

    Add quick start for ts SDK

---
+++ examples/ts/quickstart/index.ts
@@ -0,0 +1,44 @@
+import * as topology from "reduction-ts/topology";
+import * as embedded from "reduction-ts/connectors/embedded";
+import * as stdio from "reduction-ts/connectors/stdio";
+import { UInt64Codec } from "reduction-ts/state";
+import type { KeyedEvent, Subject } from "reduction-ts";
+
+function createHandler(op: topology.Operator, sink: stdio.Sink) {
+  const countSpec = new topology.ValueSpec<number>(op, "count", new UInt64Codec(), 0);
+
+  function onEvent(subject: Subject, event: KeyedEvent) {
+    const count = countSpec.stateFor(subject);
+    count.setValue(count.value + 1);
+    if (count.value % 100_000 === 0) {
+      sink.collect(subject, Buffer.from(`Count: ${count.value}`));
+    }
+  }
+
+  return {
+    onEvent,
+    onTimerExpired(subject: Subject, timer: Date) {
+      throw new Error("timers not used");
+    },
+  };
+}
+
+const job = new topology.Job({
+  workerCount: 1,
+  workingStorageLocation: "storage",
+});
+new embedded.Source(job, "source", {
+  keyEvent: () => [{
+    key: new Uint8Array(),
+    value: new Uint8Array(),
+    timestamp: new Date(),
+  }],
+  generator: "sequence",
+});
+const sink = new stdio.Sink(job, "sink");
+new topology.Operator(job, "operator", {
+  parallelism: 1,
+  handler: (op) => createHandler(op, sink),
+});
+
+job.run();
